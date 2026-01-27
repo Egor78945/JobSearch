543bdfd40a00c3de7120b04a9c225cb6acf8cde4
Egor Petrov <egor78396@gmail.com>
2026-01-27 20:52:02 +0300

    database-manager(transaction registration)

---
+++ database-manager/src/main/java/com/example/database_manager/service/user/registration/TransactionalUserRegistrationProtoServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.database_manager.service.user.registration;
+
+import com.example.database_manager.service.user.UserService;
+import com.example.database_manager.service.user.roles.UsersRolesService;
+import com.proto.user.UserProtoConfiguration;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class TransactionalUserRegistrationProtoServiceImpl extends UserRegistrationProtoServiceImpl{
+    public TransactionalUserRegistrationProtoServiceImpl(UserService<UserProtoConfiguration.UserMessage> userService, UsersRolesService<UserProtoConfiguration.LongStringMessage> usersRolesService) {
+        super(userService, usersRolesService);
+    }
+
+    @Override
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public UserProtoConfiguration.UserRegistrationMessage register(UserProtoConfiguration.UserRegistrationMessage entity) {
+        return super.register(entity);
+    }
+}
--- database-manager/src/main/java/com/example/database_manager/service/user/registration/UserRegistrationProtoServiceImpl.java
+++ database-manager/src/main/java/com/example/database_manager/service/user/registration/UserRegistrationProtoServiceImpl.java
@@ -5,9 +5,11 @@ import com.example.database_manager.service.user.UserService;
 import com.example.database_manager.service.user.roles.UsersRolesService;
 import com.example.database_manager.util.mapper.UsersRolesMapper;
 import com.proto.user.UserProtoConfiguration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class UserRegistrationProtoServiceImpl implements RegistrationService<UserProtoConfiguration.UserRegistrationMessage> {
     protected final UserService<UserProtoConfiguration.UserMessage> userService;
     protected final UsersRolesService<UserProtoConfiguration.LongStringMessage> usersRolesService;
