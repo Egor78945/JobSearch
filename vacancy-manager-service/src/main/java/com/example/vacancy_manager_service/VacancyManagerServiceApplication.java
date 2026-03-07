1bf7e463ae77d11fa76ee9268d73865ae72bcad0
Egor Petrov <egor78396@gmail.com>
2026-03-07 09:48:58 +0300

    v-m-s(first config)

--- vacancy-manager-service/src/main/java/com/example/vacancy_manager_service/VacancyManagerServiceApplication.java
+++ vacancy-manager-service/src/main/java/com/example/vacancy_manager_service/VacancyManagerServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.vacancy_manager_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VacancyManagerServiceApplication {
 
     public static void main(String[] args) {
