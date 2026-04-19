63aeaf0fa3ac54d083b2bae5752cc58424033d6e
Egor Petrov <egor78396@gmail.com>
2026-04-19 13:35:18 +0300

    web client exception handling

---
+++ vacancy-manager-service/src/main/java/com/example/vacancy_manager_service/controller/advice/CommonControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.example.vacancy_manager_service.controller.advice;
+
+import com.example.vacancy_manager_service.controller.advice.handler.CommonControllerExceptionHandler;
+import com.example.vacancy_manager_service.exception.WebClientException;
+import com.example.vacancy_manager_service.model.web.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice(annotations = CommonControllerExceptionHandler.class)
+public class CommonControllerAdvice {
+
+    @ExceptionHandler(WebClientException.class)
+    public ResponseEntity<ErrorResponse> webClientExceptionHandler(WebClientException e) {
+        HttpStatus httpStatus = HttpStatus.valueOf(e.getStatus());
+        return new ResponseEntity<>(new ErrorResponse(e.getText(), e.getMessage(), e.getStatus()), httpStatus);
+    }
+}
